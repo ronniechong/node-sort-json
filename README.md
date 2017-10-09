547f9a816808c3720dff157a500a085a055e8e38
Ronnie Chong <rchong@opentable.com>
2017-10-09 14:01:14 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# node-sort-json
+
+## Instructions
+
+
+Sort a file in absolute path `-f`
+```bash
+node ./sort.js -f path/to/file.json
+```
+
+Ignore specific keys when sorting by adding `-i` as a parameter
+```bash
+node ./sort.js -f path/to/file.json -i key1,key2,key3...
+```
---
+++ sort.js
@@ -0,0 +1,96 @@
+/**
+ * Sorts Json keys alphabetically
+ *
+ * Params:
+ * -f absolute path to JSON file. E.g. -f path/to/file.json
+ * -i ignored keys so they are not sorted. Use commas as delimiter E.g -i smartling,apply.filter
+ */
+const sortJson = require('sort-json');
+const argv = require('minimist')(process.argv.slice(2));
+const jsonFile = require('jsonfile');
+const chalk = require('chalk');
+const os = require('os');
+const _ = require('lodash');
+
+// Settings
+const delimiter = ',';
+const sortJsonOptions = {
+  ignoreCase: true,
+  reverse: false,
+};
+const jsonFileOptions = {
+  spaces: 2,
+  EOL: os.EOL,
+};
+
+// Filename
+const file = argv.f;
+
+// Reads Json file
+function readJsonFile() {
+  /* eslint-disable */
+  console.log(chalk.blue('Sorting translation...'));
+  /* eslint-enable */
+  return new Promise((resolve, reject) => {
+    try {
+      jsonFile.readFile(file, (err, obj) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(obj);
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+// Sort json except excluded keys
+function sortTranslations(obj) {
+  if (argv.i) {
+    const arrKeys = argv.i.split(delimiter);
+    const objExcluded = _.pick(obj, arrKeys);
+    const objFiltered = _.omit(obj, arrKeys);
+    return Object.assign(objExcluded, sortJson(objFiltered, sortJsonOptions));
+  }
+  // Just sort all if nothing is passed in -i
+  return sortJson(obj, sortJsonOptions);
+}
+
+// Overwrite Json file
+function saveJsonFile(obj) {
+  return new Promise((resolve, reject) => {
+    try {
+      jsonFile.writeFile(file, obj, jsonFileOptions, (err) => {
+        if (err) {
+          reject(err);
+        }
+        resolve();
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+function finishedTask() {
+   /* eslint-disable */
+   console.log(chalk.green('Sorting translation done'));
+   process.exit(0);
+   /* eslint-enable */
+}
+
+function catchError(e) {
+  /* eslint-disable */
+  console.log(chalk.red(`Error: ${e}`));
+  process.exit(1);
+   /* eslint-enable */
+}
+
+
+readJsonFile()
+  .then(sortTranslations)
+  .then(saveJsonFile)
+  .then(finishedTask)
+  .catch(catchError);
+
